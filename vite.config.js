e7d41ada2d555461e0f3558bd852b36acc150b1e
Prashanth2025 <prashanthramesh.02@gmail.com>
2025-11-12 20:25:17 +0530

    Ready for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ghPages from "vite-plugin-gh-pages";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: "/Split_and_Eat/", // ✅ Correct: only repo name, with leading and trailing slash
+});
